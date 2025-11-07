682b38a579ab1afbd8671d22aa5c45cdaeae536e
tanakamasayuki <tanakamasayuki@users.noreply.github.com>
2025-11-07 08:31:32 +0000

    deploy: 07641442da145a3243c67c3aa863192b24499cb1

--- native-arduino-core/cores/native/main.cpp
+++ native-arduino-core/cores/native/main.cpp
@@ -1,8 +1,11 @@
 #include "Arduino.h"
 
+// Provide a weak default main so user sketches can override main() if desired.
+// The linker will prefer a user-defined strong main over this weak symbol.
+int main(int argc, char **argv) __attribute__((weak));
 int main(int argc, char **argv)
 {
-    // Arduino スタイルの呼び出し
+    // Arduino-style entrypoint: call setup() once, then loop() forever.
     setup();
     for (;;)
     {
